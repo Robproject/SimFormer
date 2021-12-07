f16e922017ca45c67d6012ca7278ba4387c58753
Robproject <robirtduncan@gmail.com>
2021-12-07 11:08:21 -0800

    Consolidate Arrows

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import math, cmath
 import matplotlib.pyplot as plt
 
-def arr(x, y, dx, dy):
-    return plt.arrow(x, y, dx, dy, length_includes_head=True, head_width=.1, head_length=1, width=.05)
+def oarr(a, c):
+    if c == 'c':
+        scale = .2
+    else:
+        scale = 1
+    return plt.arrow(0, 0, a.real, a.imag, length_includes_head=True, head_width=.1 * scale , head_length=1 * scale, width=.05 * scale)
 
-def oarr(a):
-    return plt.arrow(0, 0, a.real, a.imag, length_includes_head=True, head_width=.1, head_length=1, width=.05)
-
-def oarrc(a):
-    return plt.arrow(0, 0, a.real, a.imag, length_includes_head=True, head_width=.02, head_length=.02, width=.01)
+def zarr(i, vd):
+    return plt.arrow(i.real, i.imag, vd[0], vd[1], length_includes_head=True, head_width=.1, head_length=1, width=.05, color='black'), \
+           plt.arrow(i.real + vd[0], i.imag + vd[1], vd[2], vd[3], length_includes_head=True, head_width=.1, head_length=1, width=.05, color='orange')
 
 def get_vd_dxdy(I, Z):
   vrdx = (I * Z.real).real
@@ -35,6 +37,7 @@ def get_efficiency(V1, V2, I1, I2):
 def SimForm(V_2, VA, Load, pf, LeadLag, a, Z_1, Z_2, Z_C, pos):
 
     VA = VA * Load
+
     if pf == 1:
         I_2 = complex(VA / V_2, 0)
     else:
@@ -46,7 +49,6 @@ def SimForm(V_2, VA, Load, pf, LeadLag, a, Z_1, Z_2, Z_C, pos):
             I_2 = complex(ReI_2, -ImI_2)
 
     V_2 = complex(V_2, 0)
-
     E_2 = (I_2 * Z_2) + V_2
     E_1 = a * E_2
 
@@ -60,30 +62,26 @@ def SimForm(V_2, VA, Load, pf, LeadLag, a, Z_1, Z_2, Z_C, pos):
         V_1 = I_1 * Z_1 + E_1
 
     n = get_efficiency(V_1, V_2, I_1, I_2)
-    print('n =', n * 100, "%")
+    print('n =', n*100, "%")
     VR = (abs(V_1) - a*abs(V_2))/(a*abs(V_2))
     print('VR =', VR*100, "%")
     print('V_1 =', cmath.polar(V_1), 'I_1 =', cmath.polar(I_1) )
     print('V_2 =', cmath.polar(V_2), 'I_2 =', cmath.polar(I_2))
 
-    vds = get_vd_dxdy(I_2, Z_2)
-    I2R2 = arr(V_2.real, V_2.imag, vds[0], vds[1]).set_color('black')
-    I2X2 = arr(V_2.real + vds[0], V_2.imag + vds[1], vds[2], vds[3]).set_color('orange')
+    arrow = [E_2, E_1, V_2, V_1, I_2, I_1]
+    color = ['violet', 'green', 'blue', 'red', 'orange', 'red']
+    for ar, co in zip(arrow, color):
+        oarr(ar, 'v').set_color(co)
 
-    E2 = oarr(E_2).set_color('violet')
-    E1 = oarr(E_1).set_color('green')
-    V2 = oarr(V_2).set_color('blue')
-    V1 = oarr(V_1).set_color('red')
+    vds = get_vd_dxdy(I_2, Z_2)
+    VZS = zarr(V_2, vds)
 
     vdp = get_vd_dxdy(I_1, Z_1)
-    I1R1 = arr(E_1.real, E_1.imag, vdp[0], vdp[1]).set_color('black')
-    I1X1 = arr(E_1.real+vdp[0], E_1.imag+vdp[1], vdp[2], vdp[3]).set_color('orange')
+    VZP = zarr(E_1, vdp)
 
-    I2 = oarr(I_2).set_color('orange')
-    I1 = oarr(I_1).set_color('red')
-    ICr = oarrc(I_C[1]).set_color('violet')
-    ICx = oarrc(I_C[2]).set_color('pink')
-    IC = oarrc(I_C[0]).set_color('blue')
+    ICr = oarr(I_C[1], 'c').set_color('violet')
+    ICx = oarr(I_C[2], 'c').set_color('pink')
+    IC = oarr(I_C[0], 'c').set_color('blue')
 
     plt.xlim(-5, V_1.real + 10)
     plt.ylim(- abs(I_2) , V_1.imag + 10)
@@ -92,5 +90,5 @@ def SimForm(V_2, VA, Load, pf, LeadLag, a, Z_1, Z_2, Z_C, pos):
 
 if __name__ == '__main__':
 
-    SimForm(240, 2400, .8, 1, "lag", 10, 1.5 + 2.5j, 0.02 + 0.03j, 6000 + 8000j, 'center' )
+    SimForm(120, 540, .8, .85, "lag", 2, 1.5 + 1.5j, 1.12 + 1.13j, 60 + 80j, 'center' )
 #          V2(V), |S|(VA), Load(.%%), pf(.%%), lead/lag, a, Z1, Z2, ZC, 'before' or 'center'
